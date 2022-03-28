7d910c1da50820693dcaabf7341c0b5c2e5d3a58
CameronByer <chunecake@gmail.com>
2022-03-28 15:22:47 -0400

    Improve Die drawing

--- Die.py
+++ Die.py
@@ -9,13 +9,14 @@ class Die:
         self.top = faces[0]
         self.roll()
 
-    def draw(self, screen, x, y, face_size=50):
+    def draw(self, screen, x, y, face_size=50, border=2):
         self.top.draw(screen, x, y, face_size)
 
-    def draw_expanded(self, screen, x, y, face_size=50):
-        offsets = [(1, 0), (0, 1), (1, 1), (2, 1), (1, 2), (1, 3)]
+    def draw_expanded(self, screen, x, y, face_size=50, border=2):
+        #offsets = [(1, 0), (0, 1), (1, 1), (2, 1), (1, 2), (1, 3)]
+        offsets = [(i, j) for i in range(3) for j in range(2)]
         for f in range(len(self.faces)):
-            self.faces[f].draw(screen, x+offsets[f][0]*face_size, y+offsets[f][1]*face_size)
+            self.faces[f].draw(screen, x+offsets[f][0]*(face_size-border), y+offsets[f][1]*(face_size-border))
 
     def roll(self):
         self.top = random.choice(self.faces)
@@ -28,6 +29,7 @@ class Face:
         self.item = item
         self.value = value
 
-    def draw(self, screen, x, y, face_size=50):
-        pygame.draw.rect(screen, (0, 0, 0), (x, y, face_size, face_size), 2)
-        self.item.draw(screen, x+5, y+5, face_size-10, face_size-10)
+    def draw(self, screen, x, y, face_size=50, border=2):
+        pygame.draw.rect(screen, (0, 0, 0), (x, y, face_size, face_size))
+        pygame.draw.rect(screen, (255, 255, 255), (x+border, y+border, face_size-2*border, face_size-2*border))
+        self.item.draw(screen, x+border+5, y+border+5, face_size-2*border-10, face_size-2*border-10)
