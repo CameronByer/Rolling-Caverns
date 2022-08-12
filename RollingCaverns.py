b6f66a0c3e99772a9c6bd7b1a5538151c04fe9f2
CameronByer <chunecake@gmail.com>
2022-08-12 17:19:51 -0400

    Revert "Change to be reverted"
    This reverts commit bf4cb76e131224510daf76c79f91291b04b13641.

--- RollingCaverns.py
+++ RollingCaverns.py
@@ -30,7 +30,7 @@ enemy_list = (Enemy.Antlion,
               Enemy.Vulture,
               Enemy.Zombie)
 
-player_dice = [Die.Die(0, [Die.Face(Item.Dud()) for f in range(6)]) for d in range(3)]
+player_dice = [Die.Die(0, [Die.Face(Item.Item.random_item()) for f in range(9)]) for d in range(4)]
 
 player = Player.Player(100, player_dice)
 
@@ -44,11 +44,9 @@ while running:
     clock.tick(FPS) ## will make the loop run at the same speed all the time
     enemy = enemies[0]
     for event in pygame.event.get():
-        if event.type == pygame.QUIT or not player.active:
+        if event.type == pygame.QUIT:
             running = False
         if event.type == pygame.MOUSEBUTTONDOWN:
-            if event.button == 2:
-                player.inventory_open = not player.inventory_open
             if combat:
                 if event.button == 1:
                     player.turn(enemies[0])
@@ -60,27 +58,15 @@ while running:
                 if event.button == 3: #Right Click
                     for die in player.dice:
                         if die.contains(event.pos):
-                            for other_die in player.dice:
-                                other_die.selected = False
                             die.click()
                         expandedFaceIndex = die.get_expanded_index(event.pos)
                         if expandedFaceIndex != None:
-                            for face in die.faces:
-                                face.selected = False
-                            die.faces[expandedFaceIndex].selected = True
-                            player.inventory_open = True
-                            #die.faces[expandedFaceIndex] = Die.Face(Item.Item.random_item())
-                    if player.get_inventory_index(event.pos) and player.get_selected_index():
-                        die_index, face_index = player.get_selected_index()
-                        inventory_index = player.get_inventory_index(event.pos)
-                        player.dice[die_index].faces[face_index], player.inventory[inventory_index] = player.inventory[inventory_index], player.dice[die_index].faces[face_index]
-                        player.dice[die_index].faces[face_index].selected = False
-                        player.inventory[inventory_index].selected = False
+                            die.faces[expandedFaceIndex] = Die.Face(Item.Item.random_item())
             else:
                 if event.button == 1: #Left Click
                     enemies = [enemies[0]]+enemies[2:]
                     combat = True
-                if event.button == 3: #Right Click
+                if event.button == 3: #Left Click
                     enemies = [enemies[1]]+enemies[2:]
                     combat = True
                     
@@ -103,13 +89,55 @@ pygame.quit()
 '''
 ### ROADMAP ###
 
-1) Inventory
+1) Functional Fight --- DONE ---
+    a) Player Class --- DONE ---
+    b) Item Effects --- DONE ---
+    c) Turn Resolution --- DONE ---
+    d) Fight Resolution --- DONE ---
+        i) For Enemies --- DONE ---
+        ii) For Players --- DONE ---
+    
+2) 10 Enemies --- DONE ---
+    a) Ice Slime --- DONE ---
+    b) Skeleton --- DONE ---
+    c) Hornet --- DONE ---
+    d) Blood Crawler --- DONE ---
+    e) Harpy --- DONE ---
+    f) Shark --- DONE ---
+    g) Vulture --- DONE ---
+    h) Tim --- DONE ---
+    i) Antlion --- DONE ---
+    j) Demon --- DONE ---
 
-2) Modifiable Dice from Inventory
+3) 20 Items
+    a) Copper Broadsword --- DONE ---
+    b) Iron Broadsword --- DONE ---
+    c) Silver Broadsword --- DONE ---
+    d) Gold Broadsword --- DONE ---
+    e) Wooden Bow --- DONE ---
+    f) Copper Bow --- DONE ---
+    g) Iron Bow --- DONE ---
+    h) Silver Bow --- DONE ---
+    i) Gold Bow --- DONE ---
+    j) Arrow --- DONE ---
+    k) Wand of Sparking --- DONE ---
+    l) Crimson Rod --- DONE ---
+    m) Vilethorn --- DONE ---
+    n) Space Gun --- DONE ---
+    o) Waterbolt --- DONE ---
+    p) Mana Crystal --- DONE ---
+    q) Lesser Mana Potion --- DONE ---
+    r) Lesser Healing Potion --- DONE ---
+    s) Life Crystal --- DONE ---
+    t) Wood Armor --- DONE ---
+    u) Copper Armor --- DONE ---
+    v) Iron Armor --- DONE ---
+    w) Silver Armor --- DONE ---
+    x) Gold Armor --- DONE ---
 
-3) Rework Player Death
+4) Die Customization --- DONE ---
 
-4) Enemy Loot
+5) Game Map --- DONE --
 
 ###
 '''
